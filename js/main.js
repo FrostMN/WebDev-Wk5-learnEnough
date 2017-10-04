375947e75309dc28c9aff83deb0ba1f591137fe8
Aaron <aaron@asouer.com>
2017-10-03 20:39:16 -0500

    just another dumb typo fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function clickMe() {
-    alert("inclickme");
-    modal_div = document.getElementById("modal").style.display = "block";
+    //alert("inclickme");
+    document.getElementById("modal").style.display = "block";
 }
